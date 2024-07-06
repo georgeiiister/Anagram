07f1893bade00209d26057f171ae0fe8cff56404
georgeiiister <georgeiiister@gmail.com>
2024-07-06 22:41:03 +0300

    new algoritm for function create anagram ('create_anagram_plus')

--- anagram.py
+++ anagram.py
@@ -9,7 +9,7 @@ def words() -> tuple:
             'address',)
 
 
-def main():
+def create_anagram():
     err_words = tuple()
 
     for i, word in enumerate(tuple(i.upper() for i in words())):
@@ -29,6 +29,34 @@ def main():
     print(f'do you this known words: {len(words()) - len(err_words)} from {len(words())} '
           f'error_word: {str(err_words) if len(err_words) > 0 else 0}'
           )
+
+
+def create_anagram_plus():
+    err_words = tuple()
+
+    for i, word in enumerate(tuple(i.upper() for i in words())):
+
+        new_word = ''
+        old_word = word
+        while old_word:
+            position = random.choice(range(len(old_word)))
+            new_word += old_word[position]
+
+            letters_list = list(old_word)
+            del letters_list[position]
+            old_word = ''.join(letters_list)
+
+        if input(f'>> {new_word} <<\n You this known words? '
+                 f'If "yes", type it on the keypad >>> ').upper() != word:
+            err_words += (word,)
+
+    print(f'do you this known words: {len(words()) - len(err_words)} from {len(words())} '
+          f'error_word: {str(err_words) if len(err_words) > 0 else 0}'
+          )
+
+
+def main():
+    create_anagram_plus()
 
 
 if __name__ == '__main__':
